21c058ef8c8699ccd1ed403bb805815c3ee30351
awaghadsangita <sangita.awaghad1987@gmail.com>
2019-11-09 18:19:24 +0530

    removed echo

--- employeeWageFinal.sh
+++ employeeWageFinal.sh
@@ -5,15 +5,11 @@ isFullTime=2;
 empRateHour=2000;
 empChk=$(( RANDOM%3 ));
 if [ $isFullTime -eq $empChk ]; then
-	echo "present"
 	empHrs=8;
 elif [ $isPartTime -eq $empChk ]; then
-	echo "part Time"
 	empHrs=4;
 else
-	echo  "not present"
 	empHrs=0;
 fi
 
 salary=$(( $empHrs*$empRateHour ))
-echo "Salary of employee: " $salary
