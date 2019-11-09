44d9811e8b4ad30e00769163309d85720dc22040
awaghadsangita <sangita.awaghad1987@gmail.com>
2019-11-09 18:07:37 +0530

    refactored random check to empcheck

--- employeeWageFinal.sh
+++ employeeWageFinal.sh
@@ -5,15 +5,12 @@ isFullTime=2;
 empRateHour=2000;
 random=$(( RANDOM%3 ));
 if [ $isFullTime -eq $random ]; then
-	echo "present"
+	echo "hello"
 	empHrs=8;
 elif [ $isPartTime -eq $random ]; then
-	echo "part Time"
 	empHrs=4;
 else
-	echo  "not present"
 	empHrs=0;
 fi
 
 salary=$(( $empHrs*$empRateHour ))
-echo "Salary of employee: " $salary
