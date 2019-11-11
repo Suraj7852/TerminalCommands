c25d95ba8bc9b7a5582346bf3e5fc0f87c621f59
awaghadsangita <sangita.awaghad1987@gmail.com>
2019-11-11 11:39:12 +0530

    this is proper function

--- function.sh
+++ function.sh
@@ -1,18 +1,11 @@
 #!/bin/bash -x
 
-function myfunc() {
-	echo $1
-}
-
-result="$( myfunc $(( RANDOM%3 )) )"
 isFullTime=1;
 isPartTime=2;
 empRateHrs=20;
-valid=true
-for (( counter=1; counter<=20; counter++ ))
-do
-	result=$( myfunc $(( RANDOM%3 )) )
-	case $result in
+function myfunc() {
+	echo $1
+	case $1 in
 		$isFullTime)
 		empHrs=8;;
 		$isPartTime)
@@ -20,6 +13,11 @@ do
 		*)
 		empHrs=0;;
 	esac
-	TotalHrs=$(( $empHrs+$empRateHrs ))
+TotalHrs=$(( $empHrs+$empRateHrs ))
+}
+
+for (( counter=1; counter<=20; counter++ ))
+do
+	result=$( myfunc $(( RANDOM%3 )) )
 done
 
