f7710385af78413039c989895bcaa098a84771c7
awaghadsangita <sangita.awaghad1987@gmail.com>
2019-11-11 11:24:00 +0530

    Total hours added in function

--- function.sh
+++ function.sh
@@ -4,10 +4,22 @@ function myfunc() {
 	echo $1
 }
 
-result="$( myfunc $(( RANDOM%2 )) )"
-if [ $result -eq 1 ]
-then
-	echo "sucess"
-else
-	echo "faild"
-fi
+result="$( myfunc $(( RANDOM%3 )) )"
+isFullTime=1;
+isPartTime=2;
+empRateHrs=20;
+valid=true
+for (( counter=1; counter<=20; counter++ ))
+do
+	result=$( myfunc $(( RANDOM%3 )) )
+	case $result in
+		$isFullTime)
+		empHrs=8;;
+		$isPartTime)
+		empHrs=4;;
+		*)
+		empHrs=0;;
+	esac
+	TotalHrs=$(( $empHrs+$empRateHrs ))
+done
+
