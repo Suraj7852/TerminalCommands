d486f79ba124fa6bea3e5dc6cf09fc4eff62b85a
awaghadsangita <sangita.awaghad1987@gmail.com>
2019-11-11 11:19:43 +0530

    function example

--- newForLoop.sh
+++ newForLoop.sh
@@ -1,6 +1,21 @@
 #!/bin/bash -x
-for (( counter=1; counter<=5; counter++ ))
+isFullTime=1;
+isPartTime=2;
+empRateHour=20;
+for (( counter=1; counter<=20; counter++ ))
 do
-	echo $counter
+	empChk=$(( RANDOM%3 ));
+	case $empChk in
+		$isFullTime)
+		empHrs=8;;
+		$isPartTime)
+		empHrs=4;;
+		*)
+		empHrs=0;;
+	esac
+	salary=$(( $empHrs*$empRateHour ))
+	pay=$(( $pay+$salary ))
 done
+printf "$pay"
+printf "\n"
 
